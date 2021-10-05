56911507576dd9f136dd78d63d432fbdbe172f0e
Stamen <xteam.stanly@gmail.com>
2021-10-06 00:28:43 +0200

    quote (office + ron swanson)

--- src/commands/fun/quote.js
+++ src/commands/fun/quote.js
@@ -30,9 +30,78 @@ const quoteBreakingBad = new Command(['brekingbad', 'bb'], async (message, args,
     });
 });
 
+const quoteRonSwanson = new Command(['ron', 'ronswanson', 'swanson'], async (message, args, context) => {
+    var finalJson = {
+        author: { name: "Ron Swanson", url: "https://github.com/jamesseanwright/ron-swanson-quotes" },
+        color: 0x9A252A,
+        thumbnail: { url: 'https://i.imgur.com/2uwEA6O.png' },
+        footer: getFooter(message)
+    }
+
+    var naslov = "Ron Swanson Quote"; var sadrzaj;
+
+    try {
+        const quoteJson = (await axios.get('http://ron-swanson-quotes.herokuapp.com/v2/quotes')).data[0];
+        sadrzaj = `***"${quoteJson}"***`;
+    } catch(err) {
+        console.log(err);
+        naslov = errNaslov;
+        sadrzaj = errSadrzaj;
+    }
+
+    finalJson.title = naslov;
+    finalJson.description = sadrzaj;
+
+    await message.channel.createMessage({
+        messageReference: getMessageReference(message),
+        embed: finalJson
+    });
+});
+
+const quoteOffice = new Command('office', async (message, args, context) => {
+    var finalJson = {
+        author: { name: "The Office", url: "https://www.officeapi.dev/" },
+        color: 0x1A384E,
+        thumbnail: { url: 'https://i.imgur.com/pXTNDtl.png' },
+        footer: getFooter(message)
+    }
+
+    var naslov = "The Office Quote"; var sadrzaj;
+
+    try {
+        const quoteJson = (await axios.get('https://www.officeapi.dev/api/quotes/random')).data.data;
+        console.log(quoteJson)
+        sadrzaj = `***"${quoteJson.content}"***\n- ${quoteJson.character.firstname} ${quoteJson.character.lastname}`;
+    } catch(err) {
+        console.log(err);
+        naslov = errNaslov;
+        sadrzaj = errSadrzaj;
+    }
+
+    finalJson.title = naslov;
+    finalJson.description = sadrzaj;
+
+    await message.channel.createMessage({
+        messageReference: getMessageReference(message),
+        embed: finalJson
+    });
+});
 
 const quote = new Command(['quote', 'quotes'], async (message, args, context) => {
-
-}).addSubcommand(quoteBreakingBad);
+    await message.channel.createMessage({
+        messageReference: getMessageReference(message),
+        embed: {
+            author: { name: "Nasumičan citat" },
+            color: 0xF78200,
+            thumbnail: { url: 'https://i.imgur.com/U41S13T.png' },
+            title: `Unesite vrstu citata`,
+            description: "**Vrste citata:**\n" +
+            "**• breakingbad | bb**\n" +
+            "**• ron | ronswanson | swanson**\n" +
+            "**• office**",
+            footer: getFooter(message)
+        }
+    });;
+}).addSubcommand(quoteBreakingBad).addSubcommand(quoteRonSwanson).addSubcommand(quoteOffice);
 
 module.exports = quote;
--- src/index.js
+++ src/index.js
@@ -151,7 +151,7 @@ const storage = require('node-persist');
 		};
 
 		//SIP FETCH CRON
-		const sipCronJob = cron.schedule('0 */10 * * * *', sipFetcher, { scheduled: false });
+		const sipCronJob = cron.schedule('*/10 * * * *', sipFetcher, { scheduled: false });
 		//0 */10 * * * *
 
 		//BOT JE SPREMAN
